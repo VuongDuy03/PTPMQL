5f100307d7f8ce589712463cdc85afa8c934d590
VuongDuy03 <vuongduy756@gmail.com>
2025-02-25 23:37:51 +0700

    BTH02

---
+++ BTH02/Controllers/DiemmonhocController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BTH.Models;
+
+namespace BTH.Controllers
+{
+    public class DiemmonhocController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(DiemmonhocModel ps)
+        {
+            ps.SubjectScore =((ps.DiemA*6 + ps.DiemB*4 + ps.DiemC*1)/10.0f);
+            ViewBag.Message=$"{ps.Name}, {ps.Subject},Diem la: {ps.SubjectScore:F2} ";
+            return View();
+        }
+    }
+}
---
+++ BTH02/Models/DiemmonhocModel.cs
@@ -0,0 +1,14 @@
+namespace BTH.Models
+{
+    public class DiemmonhocModel
+    {
+        public string Name{get; set;}
+        public string Subject{get; set;}
+        public float SubjectScore{get; set;}
+        public float DiemA{get; set;}
+        public float DiemB{get; set;}
+        public float DiemC{get;set;}
+
+
+    }
+}
